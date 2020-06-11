5aaa35b5aa7ec36bf7d24fa8fc27a35f9499adec
Alexandre Vilcek <vilcek@gmail.com>
2020-06-11 16:05:03 -0700

    fix joblib dependency and import

--- training_script/train_horovod.py
+++ training_script/train_horovod.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.externals import joblib
+import joblib
 import os, argparse, time, random
 import torch
 from torch.utils.data import TensorDataset, DataLoader
